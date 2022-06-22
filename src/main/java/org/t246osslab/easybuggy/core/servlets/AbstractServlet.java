266945fe2f1b4c0d11fee89af859f48c3f0457cc
Josh Newton <josh.newton@mend.io>
2022-06-22 09:43:20 -0400

    added jboss-websocket

--- src/main/java/org/t246osslab/easybuggy/core/servlets/AbstractServlet.java
+++ src/main/java/org/t246osslab/easybuggy/core/servlets/AbstractServlet.java
@@ -1,6 +1,7 @@
 package org.t246osslab.easybuggy.core.servlets;
 
 import javax.servlet.http.HttpServlet;
+import io.whitesource.cure.Encoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -91,7 +92,7 @@ public abstract class AbstractServlet extends HttpServlet {
             writer.write("</tr>");
             writer.write("</table>");
             writer.write("<hr style=\"margin-top:0px\">");
-            writer.write(htmlBody);
+            writer.write(Encoder.forHtmlContentXss(htmlBody));
             writer.write("</BODY>");
             writer.write("</HTML>");
 
